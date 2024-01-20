38314c1ca0257f29609be6674b306dc4ddc0653b
nathanielzerah <154419186+nathanielzerah@users.noreply.github.com>
2024-01-20 20:13:32 +0200

    GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -160,7 +160,7 @@
 
         for (int i = 1; i < rows - 1; i++) {
             for (int j = 1; j < cols - 1; j++) {
-                System.out.print(" " + arr[i][j] + " ");
+                System.out.print("  " + arr[i][j]);
             }
             System.out.println();
         }
